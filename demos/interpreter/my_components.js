9cf34226d5dde78d3897016d98449da29948968d
Akash Agarwal <virusofminds@gmail.com>
2020-02-13 14:00:06 +1100

    Add localStorage persistence Update stats view to show parent requirements of scope etc

--- demos/interpreter/my_components.js
+++ demos/interpreter/my_components.js
@@ -1,23 +1,29 @@
 var myApplication = {
-  myComponents: []
+  doms: [],
+  domTexts: []
 };
 
 const MY_COMPONENTS_KEY = "myComponents";
 
 function firstLoad() {
-  myApplication.myComponents = JSON.parse(localStorage.getItem(MY_COMPONENTS_KEY)) || []
+  myApplication.domTexts = localStorage.getItem(MY_COMPONENTS_KEY).split(',') || []
+  myApplication.doms = myApplication.domTexts.map(item => Blockly.Xml.textToDom(item));
 }
 
-// if (myApplication.myComponents.length === 0) firstLoad();
-
 function updateLocalStorage() {
-  localStorage.setItem(MY_COMPONENTS_KEY, getComponents());
+  localStorage.setItem(MY_COMPONENTS_KEY, myApplication.domTexts);
 }
 
 function getComponents() {
-  return myApplication.myComponents;
+  if (myApplication.doms.length === 0) firstLoad();
+
+  return myApplication.doms;
 }
 
-function addComponent(component) {
-  myApplication.myComponents.push(component);
+function addComponent(blockObj) {
+  let dom = Blockly.Xml.blockToDom(blockObj, true);
+  myApplication.doms.push(dom);
+  let domText = Blockly.Xml.domToText(dom);
+  myApplication.domTexts.push(domText);
+  updateLocalStorage();
 }
